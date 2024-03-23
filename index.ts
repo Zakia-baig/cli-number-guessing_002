889d5714a21dd792bf60cb57775cfeabe37d0f13
Zakia-Baig <zakiabaig0507@gmail.com>
2024-03-22 23:31:00 -0700

    push my cli guess game

---
+++ index.ts
@@ -0,0 +1,11 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const RandomNunber = Math.floor(Math.random() * 6 + 1);
+
+const answer = await inquirer.prompt([
+  { message: "Please guess a number between 1-6 :" , type: "numberguess", name: "Number" },
+]);
+if( answer. numberguess=== "Randomnumber"  ){  console.log("Congratulation you guesss right" ) }
+else{console.log("You guess wrong number")};
